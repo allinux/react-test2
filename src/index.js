36c14685d36c809f2af5d7bceaefe8bb272d33f0
allinux <allinux36@gmail.com>
2018-10-31 12:45:23 +0900

    a

--- src/index.js
+++ src/index.js
@@ -8,46 +8,44 @@ import MyCard from "./components/MyCard";
 import "./styles.css";
 
 function App() {
-  var inlineCss = {
-    float: "none"
-  };
+	return (
+		<div className="App">
+			<h1>Hello CodeSandbox</h1>
+			<h2>Start editing to see some magic happen!</h2>
+			<My />
+			<My2 myvalue="zzzz" />
+			<MyName name="yhjung" />
+			<div>
+				<MyBlock color="blue" bgColor="red">
+					A
+				</MyBlock>
+				<MyBlock color="yellow" bgColor="black">
+					E
+				</MyBlock>
+				<MyBlock color="orange" bgColor="gray">
+					I
+				</MyBlock>
+				<MyBlock color="cron" bgColor="brown">
+					O
+				</MyBlock>
+				<MyBlock color="cron" bgColor="brown">
+					R
+				</MyBlock>
+				{[...Array(3)].map((v, i) => (
+					<MyBlock key={i}>{v}</MyBlock>
+				))}
+			</div>
+			<div>
+				<MyProducts />
+			</div>
 
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <My />
-      <My2 myvalue="zzzz" />
-      <MyName name="yhjung" />
-      <div>
-        <MyBlock color="blue" bgColor="red">
-          A
-        </MyBlock>
-        <MyBlock color="yellow" bgColor="black">
-          E
-        </MyBlock>
-        <MyBlock color="orange" bgColor="gray">
-          I
-        </MyBlock>
-        <MyBlock color="cron" bgColor="brown">
-          O
-        </MyBlock>
-        <MyBlock color="cron" bgColor="brown">
-          R
-        </MyBlock>
-        {[...Array(3)].map((v, i) => <MyBlock key={i}>{v}</MyBlock>)}
-      </div>
-      <div>
-        <MyProducts />
-      </div>
-
-      <div className="Container">
-        <MyCard />
-        <MyCard />
-        <MyCard style={inlineCss} />
-      </div>
-    </div>
-  );
+			<div className="Container">
+				<MyCard />
+				<MyCard />
+				<MyCard />
+			</div>
+		</div>
+	);
 }
 
 const rootElement = document.getElementById("root");
