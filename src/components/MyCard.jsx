51a79a9bbbb6c68cf5e132e10d9a067fb7e8a433
allinux <allinux36@gmail.com>
2018-11-04 12:33:43 +0900

    a

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -10,37 +10,35 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 export default function ImgMediaCard(/*props*/) {
-  //const { classes } = props;
-  return (
-    <Card className="Card">
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          className="media"
-          height="180"
-          image="http://postfiles7.naver.net/MjAxNzAzMTlfMjU2/MDAxNDg5ODc4OTA0NzYy.gzqEQaijFE-G0yGCbk2S7n1iHAUJ-ygUcGUQBbKUbfkg.lntRahc92R3tOQ0qp5Jj-5v8FF59u4Ywoy0vNtCRAogg.PNG.yoray123/%EA%B3%A0%EC%96%91%EC%9D%B4.png?type=w2"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            이미지 타이틀1
-          </Typography>
-          <Typography component="p">
-            이미지 설명 이미지 카드 컴포넌트
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+	//const { classes } = props;
+	return (
+		<Card className="Card">
+			<CardActionArea>
+				<CardMedia
+					component="img"
+					alt="Contemplative Reptile"
+					className="media"
+					height="180"
+					image="http://postfiles7.naver.net/MjAxNzAzMTlfMjU2/MDAxNDg5ODc4OTA0NzYy.gzqEQaijFE-G0yGCbk2S7n1iHAUJ-ygUcGUQBbKUbfkg.lntRahc92R3tOQ0qp5Jj-5v8FF59u4Ywoy0vNtCRAogg.PNG.yoray123/%EA%B3%A0%EC%96%91%EC%9D%B4.png?type=w2"
+					title="Contemplative Reptile"
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="h2">
+						이미지 타이틀12
+					</Typography>
+					<Typography component="p">이미지 설명 이미지 카드 컴포넌트</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Button size="small" color="primary">
+					Share
+				</Button>
+				<Button size="small" color="primary">
+					Learn More
+				</Button>
+			</CardActions>
+		</Card>
+	);
 }
 
 //export default withStyles(styles)(ImgMediaCard);
